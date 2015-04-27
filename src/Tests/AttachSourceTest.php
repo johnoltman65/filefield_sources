e1e3db136b7aeaadda918a9ac13748e088768a6a
tien.xuan.vo <tien.xuan.vo@gmail.com>
2015-04-27 18:12:29 +0700

    Refactoring attach source test case.

--- src/Tests/AttachSourceTest.php
+++ src/Tests/AttachSourceTest.php
@@ -15,6 +15,78 @@ namespace Drupal\filefield_sources\Tests;
 class AttachSourceTest extends FileFieldSourcesTestBase {
 
   /**
+   * Check to see if a option is present.
+   *
+   * @param type $uri
+   *   The option to check.
+   *
+   * @return
+   *   TRUE if the option is present, FALSE otherwise.
+   */
+  public function isOptionPresent($uri) {
+    $options = $this->xpath('//select[@name=:name]/option[@value=:option]', array(
+      ':name' => $this->fieldName . '[0][filefield_attach][filename]',
+      ':option' => $uri,
+    ));
+    return isset($options[0]);
+  }
+
+  /**
+   * Upload file by 'Attach' source.
+   *
+   * @param object $file
+   *   The file object.
+   */
+  public function uploadFile($file) {
+    $edit = array(
+      $this->fieldName . '[0][filefield_attach][filename]' => $file->uri,
+    );
+    $this->drupalPostAjaxForm(NULL, $edit, $this->fieldName . '_0_attach');
+    //$this->drupalPostForm(NULL, $edit, t('Attach'));
+
+    // Ensure file is uploaded.
+    $this->assertFileUploaded($file->filename);
+  }
+
+  /**
+   * Check to see if file is uploaded.
+   *
+   * @param $filename
+   *   The file name to check.
+   */
+  public function assertFileUploaded($filename) {
+    $this->assertLink($filename);
+    $this->assertFieldByXPath('//input[@type="submit"]', t('Remove'), 'After uploading a file, "Remove" button is displayed.');
+    $this->assertNoFieldByXPath('//input[@type="submit"]', t('Attach'), 'After uploading a file, "Attach" button is no longer displayed.');
+  }
+
+  /**
+   * Remove uploaded file.
+   *
+   * @param object $file
+   *   The file object.
+   */
+  public function removeFile($file) {
+    $remove_button = $this->xpath('//input[@type="submit" and @value="' . t('Remove') . '"]');
+    $this->drupalPostAjaxForm(NULL, array(), array((string) $remove_button[0]['name'] => (string) $remove_button[0]['value']));
+
+    // Ensure file is removed.
+    $this->assertFileRemoved($file->filename);
+  }
+
+  /**
+   * Check to see if file is removed.
+   *
+   * @param type $filename
+   *   The file name to check.
+   */
+  public function assertFileRemoved($filename) {
+    $this->assertNoLink($filename);
+    $this->assertNoFieldByXPath('//input[@type="submit"]', t('Remove'), 'After clicking the "Remove" button, it is no longer displayed.');
+    $this->assertFieldByXpath('//input[@type="submit"]', t('Attach'), 'After clicking the "Remove" button, the "Attach" button is displayed.');
+  }
+
+  /**
    * Tests move file from relative path.
    *
    * Default settings: Move file from 'public://file_attach' to 'public://'.
@@ -29,46 +101,34 @@ class AttachSourceTest extends FileFieldSourcesTestBase {
       'attach' => TRUE,
     ));
 
-    // Ensure option exists.
-    $options = $this->xpath('//select[@name=:name]/option[@value=:option]', array(
-      ':name' => $this->fieldName . '[0][filefield_attach][filename]',
-      ':option' => $file->uri,
-    ));
-    $this->assertTrue(isset($options[0]), 'File option exists.');
+    // Ensure option is present.
+    $this->assertTrue($this->isOptionPresent($file->uri), 'File option exists.');
 
-    // Upload a file by 'Attach' source.
-    $edit = array(
-      $this->fieldName . '[0][filefield_attach][filename]' => $file->uri,
-    );
-    $this->drupalPostForm(NULL, $edit, t('Attach'));
+    // Upload a file.
+    $this->uploadFile($file);
 
-    // Ensure option no longer exists.
-    $options = $this->xpath('//select[@name=:name]/option[@value=:option]', array(
-      ':name' => $this->fieldName . '[0][filefield_attach][filename]',
-      ':option' => $file->uri,
-    ));
-    $this->assertFalse(isset($options[0]), 'File option no longer exists.');
-
-    // Ensure file is uploaded.
-    $this->assertLink($file->filename);
-    $this->assertFieldByXPath('//input[@type="submit"]', t('Remove'), t('After uploading a file, "Remove" button displayed.'));
-    $this->assertNoFieldByXPath('//input[@type="submit"]', t('Attach'), t('After uploading a file, "Attach" button is no longer displayed.'));
+    // Ensure option is no longer present.
+    $this->assertFalse($this->isOptionPresent($file->uri), 'File option no longer exists.');
 
     // Ensure file is moved.
     $this->assertFalse(is_file($file->uri), 'Source file has been removed.');
     $this->assertTrue(is_file($dest_uri), 'Destination file has been created.');
 
-    // Remove uploaded file.
-    $remove_button = $this->xpath('//input[@type="submit" and @value="' . t('Remove') . '"]');
-    $this->drupalPostAjaxForm(NULL, array(), array((string) $remove_button[0]['name'] => (string) $remove_button[0]['value']));
+    $this->removeFile($file->filename);
 
     // Ensure empty message exists.
     $this->assertText('There currently are no files to attach.', "'No files to attach' message exists.");
+  }
 
-    // Ensure file is removed.
-    $this->assertNoLink($file->filename);
-    $this->assertNoFieldByXPath('//input[@type="submit"]', t('Remove'), 'After clicking the "Remove" button, it is no longer displayed.');
-    $this->assertFieldByXpath('//input[@type="submit"]', t('Attach'), 'After clicking the "Remove" button, the "Attach" button is displayed.');
+  /**
+   * Calculate custom absolute path.
+   */
+  public function getCustomAttachPath() {
+    $path = drupal_realpath(file_default_scheme() . '://');
+    $path = str_replace(realpath('./'), '', $path);
+    $path = ltrim($path, '/');
+    $path = $path . '/custom_file_attach';
+    return $path;
   }
 
   /**
@@ -77,11 +137,7 @@ class AttachSourceTest extends FileFieldSourcesTestBase {
    * Copy file from 'sites/default/files/custom_file_attach' to 'public://'.
    */
   public function testCopyFileFromAbsolutePath() {
-    // Calculate custom absolute path.
-    $path = drupal_realpath(file_default_scheme() . '://');
-    $path = str_replace(realpath('./'), '', $path);
-    $path = ltrim($path, '/');
-    $path = $path . '/custom_file_attach';
+    $path = $this->getCustomAttachPath();
 
     // Create test file.
     $file = $this->createTemporaryFile($path);
@@ -96,38 +152,22 @@ class AttachSourceTest extends FileFieldSourcesTestBase {
       'attach' => TRUE,
     ));
 
-    // Ensure option exists.
-    $options = $this->xpath('//select[@name=:name]/option[@value=:option]', array(
-      ':name' => $this->fieldName . '[0][filefield_attach][filename]',
-      ':option' => $file->uri,
-    ));
-    $this->assertTrue(isset($options[0]), 'File option exists.');
+    // Ensure option is present.
+    $this->assertTrue($this->isOptionPresent($file->uri), 'File option exists.');
 
-    // Upload a file by 'Attach' source.
-    $edit = array(
-      $this->fieldName . '[0][filefield_attach][filename]' => $file->uri,
-    );
-    $this->drupalPostForm(NULL, $edit, t('Attach'));
+    // Upload a file.
+    $this->uploadFile($file);
 
-    // Ensure file is uploaded.
-    $this->assertLink($file->filename);
-    $this->assertFieldByXPath('//input[@type="submit"]', t('Remove'), t('After uploading a file, "Remove" button displayed.'));
-    $this->assertNoFieldByXPath('//input[@type="submit"]', t('Attach'), t('After uploading a file, "Attach" button is no longer displayed.'));
+    // Ensure option is no longer present.
+    $this->assertFalse($this->isOptionPresent($file->uri), 'File option no longer exists.');
 
     // Ensure file is copied.
     $this->assertTrue(is_file($file->uri), 'Source file still exists.');
     $this->assertTrue(is_file($dest_uri), 'Destination file has been created.');
 
-    // Remove uploaded file.
-    $remove_button = $this->xpath('//input[@type="submit" and @value="' . t('Remove') . '"]');
-    $this->drupalPostAjaxForm(NULL, array(), array((string) $remove_button[0]['name'] => (string) $remove_button[0]['value']));
+    $this->removeFile($file->filename);
 
-    // Ensure there are no files to attach.
+    // Ensure there are files to attach.
     $this->assertNoText('There currently are no files to attach.', "'No files to attach' message does not exist.");
-
-    // Ensure file is removed.
-    $this->assertNoLink($file->filename);
-    $this->assertNoFieldByXPath('//input[@type="submit"]', t('Remove'), 'After clicking the "Remove" button, it is no longer displayed.');
-    $this->assertFieldByXpath('//input[@type="submit"]', t('Attach'), 'After clicking the "Remove" button, the "Attach" button is displayed.');
   }
 }
